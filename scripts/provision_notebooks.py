e6953e3ecce7df1afdf748659626e7ae6bbd30d0
stuckerj1 <116856975+stuckerj1@users.noreply.github.com>
2025-10-13 16:52:35 -0600

    Add 'pass' statement to platform_metadata and name file .platform

--- scripts/provision_notebooks.py
+++ scripts/provision_notebooks.py
@@ -71,9 +71,9 @@ platform_metadata = (
     "# META     }\n"
     "# META   }\n"
     "# META }\n"
-    "\n"
+    "pass\n"
 )
-platform_metadata += "\npass  # Required to make this a valid Python file"
+
 with open(platform_py_path, "w", encoding="utf-8") as f:
     f.write(platform_metadata)
 print("----- platform_metadata.py contents -----")
